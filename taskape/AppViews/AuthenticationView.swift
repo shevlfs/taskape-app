68309ad66c9fb88a56ac5ffc43d042ee56338618
theodor shevlyakov <60010221+shevlfs@users.noreply.github.com>
2025-01-10 04:25:13 +0300

    lottie added
    fighting with figma to export my genius animation to lottie

--- taskape/AppViews/AuthenticationView.swift
+++ taskape/AppViews/AuthenticationView.swift
@@ -5,11 +5,19 @@
 //  Created by shevlfs on 1/7/25.
 //
 
+import Lottie
 import SwiftUI
 
 struct AuthenticationView: View {
+    @State private var isPlaying: Bool = true
+
     var body: some View {
-        Text("")
+        VStack {
+            Text("Authentication")
+            LottieView(animation: .named("taskape_landing"))
+                .playing(loopMode: .loop)
+                .frame(width: 200, height: 200)
+        }
     }
 }
 
