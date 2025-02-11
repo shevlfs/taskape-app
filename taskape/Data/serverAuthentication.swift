18206e1f14e74a66144feee171dc6d20ebe1d9ef
Theodor Shevlyakov <shevlfs@icloud.com>
2025-02-12 00:49:09 +0300

    ping

--- taskape/Data/serverAuthentication.swift
+++ taskape/Data/serverAuthentication.swift
@@ -5,11 +5,29 @@
 //  Created by shevlfs on 1/27/25.
 //
 
+import Alamofire
 import Foundation
 import SwiftData
 
 func serverHandShake() async -> Bool {
-    return true
+    do {
+        let result = await AF.request("http://localhost:8080/ping")
+            .validate()
+            .serializingData()
+            .response
+
+        if let error = result.error {
+            print("Error: \(error.localizedDescription)")
+            return false
+        }
+
+        if let data = result.data {
+            print(
+                "Ping response: \(String(data: data, encoding: .utf8) ?? "Invalid data")"
+            )
+        }
+        return true
+    }
 }
 
 func sendVerificationCode(phoneNumber: String, country_code: String) {
