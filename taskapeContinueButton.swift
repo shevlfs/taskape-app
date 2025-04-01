9d2e46305ba52e9edf807f8225ec3683c3e3bff7
Theodor Shevlyakov <shevlfs@icloud.com>
2025-04-01 20:03:22 +0300

    sadf

--- taskapeContinueButton.swift
+++
@@ -1,7 +0,0 @@
-struct taskapeContinueButton: View {
-    var body: some View {
-        Text("continue").padding(20)
-                .font(.pathway(25))
-                .background(Rectangle().fill(.ultraThinMaterial)).cornerRadius(30).frame(minWidth: 200)
-    }
-}
